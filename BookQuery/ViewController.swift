11490ae0aed3c951f69d876aae3b70c441d3e65d
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-08-16 11:06:04 -0500

    Building the interface and add action for search key

--- BookQuery/ViewController.swift
+++ BookQuery/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var isbnBook: UITextField!
+    
+    @IBAction func primaryButtonAction(sender: AnyObject) {
+        print(isbnBook.text!)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
