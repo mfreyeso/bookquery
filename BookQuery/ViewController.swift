60b075d13ac81897c2e70be3a37070c1543eebf9
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-08-16 18:08:03 -0500

    Add logic for get the book data from isbn code add controls for blank values and protocols for delegate

--- BookQuery/ViewController.swift
+++ BookQuery/ViewController.swift
@@ -8,22 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var isbnBook: UITextField!
+    @IBOutlet weak var textResult: UITextView!
     
-    @IBAction func primaryButtonAction(sender: AnyObject) {
-        print(isbnBook.text!)
+    @IBAction func textFieldDoneEditing(sender:UITextField){
+        sender.resignFirstResponder()
     }
 
+    @IBAction func backgroundTap(sender:UIControl){
+        isbnBook.resignFirstResponder()
+    }
+    
+    @IBAction func primaryButtonAction(sender: AnyObject) {
+        let isbn :String = isbnBook.text!
+        if isbn != "" {
+            textResult.text = queryBookDetails(isbn)
+        }else{
+            let alertController = UIAlertController(title: "Error", message: "You must write the ISBN code", preferredStyle: UIAlertControllerStyle.Alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: {(alert :UIAlertAction!) in
+                print("OK button tapped")
+            })
+            alertController.addAction(okAction)
+            
+            presentViewController(alertController, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        isbnBook.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func queryBookDetails(isbn:String) -> String{
+        do{
+            let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + isbn
+            let url = NSURL(string: urls)
+            let data:NSData? = NSData(contentsOfURL: url!)
+            let result = String(NSString(data: data!, encoding: NSUTF8StringEncoding)!)
+            return result
+        }
+        
+        
+        
     }
 
 
